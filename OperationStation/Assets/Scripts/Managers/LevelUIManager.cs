28751814249c095bbf37d785db070ceff2904e97
Alex <alexander.james.swanson@gmail.com>
2025-08-09 00:09:24 -0400

    Started on Win Menu

---
+++ OperationStation/Assets/Scripts/Managers/LevelUIManager.cs
@@ -0,0 +1,56 @@
+using TMPro;
+using UnityEngine;
+
+public class LevelUIManager : MonoBehaviour
+{
+    public static LevelUIManager instance { get; private set; }
+
+    [Header("UI Elements")]
+    [SerializeField] GameObject menuActive;
+    [SerializeField] GameObject menuPause;
+    [SerializeField] GameObject menuWin;
+    [SerializeField] GameObject menuLose;
+
+    private bool isPaused;
+    private float timescaleOrig;
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(instance);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void statePause()
+    {
+        isPaused = !isPaused;
+        Time.timeScale = 0;
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    public void stateUnpause()
+    {
+        isPaused = !isPaused;
+        Time.timeScale = timescaleOrig;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        menuActive.SetActive(false);
+        menuActive = null;
+    }
+}
