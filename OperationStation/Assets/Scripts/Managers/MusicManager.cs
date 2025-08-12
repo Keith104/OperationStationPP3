1fd6a264f105bf7f17375634cc446ac3a6329184
Alex <alexander.james.swanson@gmail.com>
2025-08-12 16:08:47 -0400

    Fading music In/Out

--- OperationStation/Assets/Scripts/Managers/MusicManager.cs
+++ OperationStation/Assets/Scripts/Managers/MusicManager.cs
@@ -5,6 +5,8 @@ public class MusicManager : MonoBehaviour
     [SerializeField] AudioSource outgoingSource;
     [SerializeField] AudioSource incomingSource;
     [SerializeField] AudioClip[] musicClips;
+
+    [SerializeField] bool fadeNewClipIn;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -14,17 +16,40 @@ public class MusicManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        NearEnd();
+        if (fadeNewClipIn == true)
+        {
+            CrossFade();
+        }
     }
 
-    void FadeIn()
+    void NearEnd()
     {
-        if(incomingSource.volume < 1)
-            incomingSource.volume += 0.1f;
+        float length = outgoingSource.clip.length;
+        if (outgoingSource.time > length - 1)
+            fadeNewClipIn = true;
     }
 
-    void FadeOut()
+    void CrossFade()
     {
 
+        if (incomingSource.volume == 0)
+            incomingSource.Play();
+
+        if (incomingSource.volume < 1)
+            incomingSource.volume += 0.001f;
+        if (outgoingSource.volume > 0)
+            outgoingSource.volume -= 0.001f;
+
+        if(incomingSource.volume == 1 && outgoingSource.volume == 0)
+        {
+            outgoingSource.volume = 1;
+            incomingSource.volume = 0;
+
+            outgoingSource.clip = incomingSource.clip;
+            outgoingSource.Play();
+            outgoingSource.time = incomingSource.time;
+            fadeNewClipIn = false;
+        }
     }
 }
