8141e60209c02b00a3da40cb301375e44fe07d43
Alex <alexander.james.swanson@gmail.com>
2025-08-18 17:00:51 -0400

    Depending on how loud the asteroid hit is changes the volume

--- OperationStation/Assets/Scripts/Sound/AsteroidSoundTrigger.cs
+++ OperationStation/Assets/Scripts/Sound/AsteroidSoundTrigger.cs
@@ -7,11 +7,13 @@ public class AsteroidSoundTrigger : MonoBehaviour
     [Header("Sound")]
     [SerializeField] SoundBank soundBank;
     [SerializeField] AudioSource audioSource;
+    [SerializeField] Rigidbody mainRB;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log("Asteroid Collision");
         soundBank.SetSourceToRandomClip();
+        audioSource.volume = mainRB.linearVelocity.magnitude;
         audioSource.Play();
     }
 }
