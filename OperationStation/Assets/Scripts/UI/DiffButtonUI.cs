4f5833b5ee8a7c30be5e551e43f1ba7a88ce3f7e
KeithJanish <keithjanish18@gmail.com>
2025-08-22 22:25:12 -0400

    Update DiffButtonUI.cs

--- OperationStation/Assets/Scripts/UI/DiffButtonUI.cs
+++ OperationStation/Assets/Scripts/UI/DiffButtonUI.cs
@@ -1,131 +1,112 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 using TMPro;
 
 public class DifficultyButtonUI : MonoBehaviour,
-    IPointerEnterHandler, IPointerExitHandler, ISelectHandler, IDeselectHandler, IPointerClickHandler
+    IPointerEnterHandler, IPointerExitHandler,
+    ISelectHandler, IDeselectHandler, IPointerClickHandler
 {
     public DifficultySO difficulty;
 
     [SerializeField] private Image buttonBorder;
     [SerializeField] private Color idleBorderColor = Color.white;
 
-    [SerializeField] private TMP_Text descriptionText;   // shared
-    [SerializeField] private Image descriptionBorder;    // shared
+    [SerializeField] private TMP_Text descriptionText;
+    [SerializeField] private Image descriptionBorder;
     [SerializeField] private Color descriptionIdleColor = Color.white;
 
-    private bool isHovered;
-    private static int hoverCount = 0; // how many difficulty buttons are currently hovered
+    static readonly List<DifficultyButtonUI> instances = new List<DifficultyButtonUI>();
+    static int hoverCount = 0;
+    static GameObject lastSelectedGO;
+    bool isHovered;
 
-    void Awake()
+    void OnEnable()
     {
+        if (!instances.Contains(this)) instances.Add(this);
         SetBorder(idleBorderColor);
-        SetDescription(null);
-        SyncWithSelection();
+        ApplyGlobal();
     }
 
-    // --------- POINTER ----------
-    public void OnPointerEnter(PointerEventData eventData)
+    void OnDisable()
+    {
+        instances.Remove(this);
+        ApplyGlobal();
+    }
+
+    void Update()
+    {
+        var cur = EventSystem.current ? EventSystem.current.currentSelectedGameObject : null;
+        if (cur != lastSelectedGO)
+        {
+            lastSelectedGO = cur;
+            ApplyGlobal();
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData e)
     {
         if (!isHovered) { isHovered = true; hoverCount++; }
-        ShowActive();
+        ApplyGlobal();
     }
 
-    public void OnPointerExit(PointerEventData eventData)
+    public void OnPointerExit(PointerEventData e)
     {
         if (isHovered) { isHovered = false; hoverCount = Mathf.Max(0, hoverCount - 1); }
-
-        if (IsSelected())
-        {
-            ShowActive();
-        }
-        else
-        {
-            SetBorder(idleBorderColor);
-            EnsureDefaultsIfNoHoverOrSelection();
-        }
+        ApplyGlobal();
     }
 
-    // --------- SELECT / DESELECT ----------
-    public void OnSelect(BaseEventData eventData)
-    {
-        DifficultyManager.instance.SetDifficulty(difficulty);
-        ShowActive();
-        RefreshAllBorders();
-    }
-
-    public void OnDeselect(BaseEventData eventData)
-    {
-        if (!isHovered)
-        {
-            SetBorder(idleBorderColor);
-            EnsureDefaultsIfNoHoverOrSelection();
-        }
-    }
-
-    // --------- CLICK makes this the selected UI element too ----------
-    public void OnPointerClick(PointerEventData eventData)
+    public void OnPointerClick(PointerEventData e)
     {
         EventSystem.current?.SetSelectedGameObject(gameObject);
-        OnSelect(null);
+        DifficultyManager.instance.SetDifficulty(difficulty);
+        ApplyGlobal();
     }
 
-    // --------- Helpers ----------
-    private bool IsSelected()
+    public void OnSelect(BaseEventData e)
     {
-        return DifficultyManager.instance.GetDifficulty() == difficulty;
+        DifficultyManager.instance.SetDifficulty(difficulty);
+        ApplyGlobal();
     }
 
-    private void ShowActive()
+    public void OnDeselect(BaseEventData e)
     {
-        SetBorder(difficulty.uiColor);
-        SetDescription(difficulty);
+        ApplyGlobal();
     }
 
-    // If NO difficulty button is selected and NONE are hovered -> reset to defaults.
-    private void EnsureDefaultsIfNoHoverOrSelection()
+    void ApplyGlobal()
     {
-        if (hoverCount > 0) return;
-
-        var selObj = EventSystem.current ? EventSystem.current.currentSelectedGameObject : null;
-        var selBtn = selObj ? selObj.GetComponent<DifficultyButtonUI>() : null;
-
-        if (selBtn == null)
+        DifficultySO active = null;
+        for (int i = 0; i < instances.Count; i++)
+            if (instances[i].isHovered) { active = instances[i].difficulty; break; }
+        if (active == null)
         {
-            DifficultyManager.instance.SetDifficulty(null); // clear last selection
-            SetDescription(null);
-
-            var all = FindObjectsByType<DifficultyButtonUI>(FindObjectsSortMode.None);
-            foreach (var b in all) b.SetBorder(b.idleBorderColor);
+            var go = EventSystem.current ? EventSystem.current.currentSelectedGameObject : null;
+            var sel = go ? go.GetComponent<DifficultyButtonUI>() : null;
+            if (sel) active = sel.difficulty;
         }
-        else
+
+        var borders = new HashSet<Image>();
+        var descBorders = new HashSet<Image>();
+        var descTexts = new HashSet<TMP_Text>();
+        for (int i = 0; i < instances.Count; i++)
         {
-            selBtn.ShowActive(); // another difficulty button is selected
+            if (instances[i].buttonBorder) borders.Add(instances[i].buttonBorder);
+            if (instances[i].descriptionBorder) descBorders.Add(instances[i].descriptionBorder);
+            if (instances[i].descriptionText) descTexts.Add(instances[i].descriptionText);
         }
+
+        Color bcol = active ? active.uiColor : (instances.Count > 0 ? instances[0].idleBorderColor : Color.white);
+        foreach (var b in borders) b.color = bcol;
+
+        foreach (var t in descTexts) t.text = active ? active.difficultyDescription : string.Empty;
+        Color dcol = active ? active.uiColor : (instances.Count > 0 ? instances[0].descriptionIdleColor : Color.white);
+        foreach (var ib in descBorders) ib.color = dcol;
     }
 
-    private void RefreshAllBorders()
-    {
-        var all = FindObjectsByType<DifficultyButtonUI>(FindObjectsSortMode.None);
-        foreach (var b in all) b.SyncWithSelection();
-    }
-
-    public void SyncWithSelection()
-    {
-        if (isHovered || IsSelected()) SetBorder(difficulty.uiColor);
-        else SetBorder(idleBorderColor);
-        // Description is controlled by hover/select so we don't flicker it here.
-    }
-
-    private void SetBorder(Color c)
+    void SetBorder(Color c)
     {
         if (buttonBorder) buttonBorder.color = c;
-    }
-
-    private void SetDescription(DifficultySO d)
-    {
-        if (descriptionText) descriptionText.text = d ? d.difficultyDescription : string.Empty;
-        if (descriptionBorder) descriptionBorder.color = d ? d.uiColor : descriptionIdleColor;
     }
 }
