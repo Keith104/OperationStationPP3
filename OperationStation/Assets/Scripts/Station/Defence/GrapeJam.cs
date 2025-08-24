0db8ac82e0be5243897718c54ccaa1269f1d5d6e
Alex <alexander.james.swanson@gmail.com>
2025-08-24 16:31:49 -0400

    made the modifications to grape jam

--- OperationStation/Assets/Scripts/Station/Defence/GrapeJam.cs
+++ OperationStation/Assets/Scripts/Station/Defence/GrapeJam.cs
@@ -4,13 +4,25 @@ using UnityEngine.AI;
 
 public class GrapeJam : MonoBehaviour
 {
+    [Header("Timers")]
+    [SerializeField] float jamDelay;
     [SerializeField] float upTime; // time in seconds, if nothing is set then timer is set to 5 min
+
+    [Header("Fragment")]
     [SerializeField] GameObject fragmentModel;
+
+    [Header("Jam")]
+    [SerializeField] GameObject jamModel;
+    [SerializeField] GameObject[] otherModels;
+    private Material jamMaterial;
+    [SerializeField] float fadeInDuration;
+    [SerializeField] float fadeOutDuration; 
 
     private bool hasItTriggeredOnce = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        jamMaterial = jamModel.GetComponent<Renderer>().material;
         if (upTime <= 0) upTime = 300f;
         
         if (fragmentModel != null)
@@ -26,17 +38,62 @@ public class GrapeJam : MonoBehaviour
     IEnumerator DelayBeforeDestroy()
     {
         
-        yield return new WaitForSeconds(upTime);
+        yield return new WaitForSeconds(jamDelay);
+
+        foreach (GameObject model in otherModels)
+            model.SetActive(false);
+
         fragmentModel.SetActive(true);
+        jamModel.SetActive(true);
+        StartCoroutine(FadeJamIn());
+    }
+
+    IEnumerator FadeJamIn()
+    {        
+        Color jamColor = jamMaterial.color;
+
+        jamColor.a = 0;
+        jamMaterial.color = jamColor;
+
+        float timePassed = 0;
+        while (timePassed < fadeInDuration)
+        {
+            timePassed += Time.deltaTime;
+            jamColor.a = Mathf.Lerp(0, 1, timePassed / fadeInDuration);
+            jamMaterial.color = jamColor;
+            yield return null;
+        }
+
+        StartCoroutine(FadeJamOut());
+    }
+
+    IEnumerator FadeJamOut()
+    {
+        Color jamColor = jamMaterial.color;
+
+        jamColor.a = 0;
+        jamMaterial.color = jamColor;
+
+        float timePassed = 0;
+        while (timePassed < fadeOutDuration)
+        {
+            timePassed += Time.deltaTime;
+            jamColor.a = Mathf.Lerp(1, 0, timePassed / fadeOutDuration);
+            jamMaterial.color = jamColor;
+            yield return null; 
+        }
+
         Destroy(gameObject);
     }
+
+
     private void OnTriggerEnter(Collider other)
     {
         NavMeshAgent enemyNAv = other.GetComponent<NavMeshAgent>();
         EnemyAI enemy = other.GetComponent<EnemyAI>();
         if (enemyNAv != null && enemy.isSlowed == false)
         {
-            enemyNAv.speed = enemyNAv.speed / 2;
+            enemyNAv.speed /= 2;
             enemy.isSlowed = true;
         }
 
@@ -49,7 +106,7 @@ public class GrapeJam : MonoBehaviour
         EnemyAI enemy = other.GetComponent<EnemyAI>();
         if (enemyNAv != null && enemy.isSlowed == true)
         {
-            enemyNAv.speed = enemyNAv.speed * 2;
+            enemyNAv.speed *= 2;
             enemy.isSlowed = false;
 
         }
